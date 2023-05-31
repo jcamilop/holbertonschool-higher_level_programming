29cc588fbee0819292f1f8414e6be7564633d5fa
jcamilop <https://github.com/jcamilop>
2023-05-31 18:18:50 -0500

    python

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = place.split('')
+print(str[5]''str[6]''str[7]''str[13]''str[0]
+      
