680fbc93db556e78f0bfbed510da59bbb6c66e00
jcamilop <https://github.com/jcamilop>
2023-06-01 23:25:41 -0500

    digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-#!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
 last_number = abs(number) % 10
 
 if number < 0:
