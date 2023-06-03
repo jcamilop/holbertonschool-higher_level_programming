dda93511342421e8bbac931b26152564696719eb
jcamilop <https://github.com/jcamilop>
2023-06-03 00:05:24 -0500

    args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
+import sys
 
-    import sys
-    args = sys.argv[1:]
-    num_args = len(args)
-
-    if num_args == 0:
-        print(f"{num_args} arguments.")
-    elif num_args == 1:
-        print(f"{num_args} argument:")
+if __name__ == "__main__":
+    count = 0
+    size = len(sys.argv) - 1
+    if size == 0:
+        print("0 arguments.")
+    elif size == 1:
+        print("{} argument:".format(size))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
-        print(f"{num_args} arguments:")
-
-    for i, arg in enumerate(args):
-        print(f"{i + 1}: {arg}")
+        print("{} arguments:".format(size))
+        for i in sys.argv[1:]:
+            count += 1
+            print("{}: {}".format(count, i))
