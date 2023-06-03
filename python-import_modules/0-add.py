b83db085f56cf5a88696245ea90c91be54c9857e
jcamilop <https://github.com/jcamilop>
2023-06-03 00:21:14 -0500

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,5 +2,7 @@
 from add_0 import add
 a = 1
 b = 2
+
 if __name__ == "__main__":
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
