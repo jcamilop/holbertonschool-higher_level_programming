816315b44f70472b2f061001389638d70712a8c2
jcamilop <https://github.com/jcamilop>
2023-06-15 11:21:55 -0500

    Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""empty class Square """
+
+
+class Square:
+    """Class for define a square
+    Private instance attribute: size
+    """
+
+    def __init__(self, size):
+        self.__size = size
