499589c53aeafb970ceaa2ad17d3258700214cf5
jcamilop <https://github.com/jcamilop>
2023-06-15 11:27:55 -0500

    Size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class Square """
+
+
+class Square:
+    """ Class square"""
+
+    def __init__(self, size=0):
+        """ Parameter: size of square"""
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
