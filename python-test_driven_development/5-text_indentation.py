d7d4bf5bce0fd64e19539fccaa9f148fcb315e0c
jcamilop <https://github.com/jcamilop>
2023-06-16 11:39:47 -0500

    Max integer - Unittest

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" module for import text indentation i"""
+
+
+def text_indentation(text):
+    """ split strings
+    Text: text to split"""
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    s = text[:]
+
+    for d in ".?:":
+        list_text = s.split(d)
+        s = ""
+        for i in list_text:
+            i = i.strip(" ")
+            s = i + d if s is "" else s + "\n\n" + i + d
+
+    print(s[:-3], end="")
