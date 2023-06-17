f5107fd29b1e2339e31a2d2a046d70d20b93472e
jcamilop <https://github.com/jcamilop>
2023-06-16 23:16:07 -0500

    Detect instance deletion

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Module 1-rectangle"""
+
+
+class Rectangle:
+    """Rectangle class defined by width and height."""
+
+    def __init__(self, width=0, height=0):
+        """clase rectangulo con base y altura"""
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        """ dibujar el rectangulo"""
+        if self.__height == 0 or self.__width == 0:
+            return ''
+        rec_str = ''
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rec_str += '#'
+            rec_str += '\n'
+        return rec_str[:-1]
+
+    def __repr__(self):
+        """Dibujar una instancia de rectangulo"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Borrar instancia de un rectangulo"""
+        print("Bye rectangle...")
+
+    @property
+    def width(self):
+        """obtiene la altura del rectangulo"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """establecer altura del rectangulo"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Obtener base del rectangulo"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Establecer base del rectangulo"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Retornar area del rectungulo"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Retornar perimetro del rectangulo """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
