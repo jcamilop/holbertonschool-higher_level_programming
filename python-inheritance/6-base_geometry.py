2861d62de869875a7342f463bc7b038b43738262
jcamilop <https://github.com/jcamilop>
2023-06-22 23:09:07 -0500

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Define class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Represent class"""
+
+    def area(self):
+        """Make exception"""
+        raise Exception("area() is not implemented")
