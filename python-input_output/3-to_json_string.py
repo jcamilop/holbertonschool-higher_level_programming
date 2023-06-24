ec35330c0456ae0c2e00b0cfaeb7ef365463103f
jcamilop <https://github.com/jcamilop>
2023-06-23 22:45:34 -0500

    To JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module fot to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a object"""
+    return json.dumps(my_obj)
